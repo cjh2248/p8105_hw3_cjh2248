ad1026aa5a733938d4250a657fdf88a8b90da1f6
Cindy Ho <cindyho@Cindys-MBP.lan>
2024-10-09 16:56:47 -0400

    snowfall values count + loading all other datasets in this package

--- p8105.datasets/data-raw/instacart.R
+++
@@ -1,9 +0,0 @@
-###########################################################
-# File imports and organizes rest_inspec data
-###########################################################
-
-library(readr)
-
-instacart <- read_csv(here::here("data-raw", "instacart_train_data.csv.zip"))
-
-devtools::use_data(instacart, overwrite = TRUE)
--- p8105.datasets/data-raw/ny_noaa.R
+++
@@ -1,9 +0,0 @@
-###########################################################
-# File imports and organizes ny_noaa data
-###########################################################
-
-library(readr)
-
-ny_noaa <- read_csv(here::here("data-raw", "nynoaadat.zip"))
-
-devtools::use_data(ny_noaa, overwrite = TRUE)
